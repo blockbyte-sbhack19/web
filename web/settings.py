c1c34d40d391668e921d955247d894f37b03099f
Cédric Walter <cedric.walter@gmail.com>
2019-06-22 11:45:46 +0200

    fix readme

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q=2p=_@dbq$mqik+#t8yjho@h$0m))q8l-olmz+53ai5_qk-@b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
